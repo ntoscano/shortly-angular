eddaaae290847250b71bb4ce52dbde223b04eb47
ntoscano <Nicolas.Toscano@gmail.com>
2015-08-02 10:35:00 -0700

    successful url validation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,17 +1,18 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, $http, Links) {
-  $scope.link = {};
-  $scope.addLink = function(link){
-    return $http({
-      method: 'POST',
-      url: '/api/links',
-      data: {
-        url: link
-      }
-    })
+  $scope.link = {
+    link : 'http://test.com'
+  };
+  $scope.addLink = function(){
+    if($scope.link.link){
+      return $http({
+        method: 'POST',
+        url: '/api/links',
+        data: {
+          url: $scope.link.link
+        }
+      })
+    }
   }
-  $scope.addLink('http://www.google.com').then(function(resp){
-      console.log('resp', resp);
-    })
 });
